9b1660525251ce47cfcbb14cf08b509c665ec220
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-01-22 19:11:00 +0200

    Corrects exercise 1

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,35 +1,31 @@
 package es1;
 
+import es1.exceptions.NotValidIntegerException;
+
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         customSet();
     }
 
     public static void customSet() {
         HashSet<String> stringSet = new HashSet<>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Inserisci un numero intero N: ");
-        if (!scanner.hasNextInt() || scanner.nextInt() <= 0) {
-            System.out.println("Errore: il numero inserito non è valido.");
-        } else {
-            int N = scanner.nextInt();
-            for (int i = 1; i <= N; i++) {
-                System.out.println("Inserisci una parola: ");
-                if (!stringSet.add(scanner.next())) {
-                    System.out.println("Errore: la parola " + scanner.next() + " è già presente nel set.");
-                } else {
-                    stringSet.add(scanner.next());
-                }
+        boolean correctInput = false;
+        while (!correctInput) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Inserisci un numero intero N: ");
+            try {
+                int N = Integer.parseInt(scanner.nextLine());
+                if (N <= 0) throw new NotValidIntegerException("Il numero deve essere maggiore di 0");
+                correctInput = true;
+            } catch (NotValidIntegerException e) {
+                System.out.println(e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Il valore inserito non è un numero intero");
             }
-        }
-        scanner.close();
-        System.out.println("Il set contiene " + stringSet.size() + " elementi.");
-        System.out.println("Elementi del set: ");
-        for (String s : stringSet) {
-            System.out.println(s);
         }
     }
 }
---
+++ src/es1/exceptions/NotValidIntegerException.java
@@ -0,0 +1,7 @@
+package es1.exceptions;
+
+public class NotValidIntegerException extends Exception {
+    public NotValidIntegerException(String message) {
+        super(message);
+    }
+}
